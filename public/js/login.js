04c882df290432c644793d85a2745f99d55e7480
Marius <marius@example.com>
2022-05-18 12:01:49 +0300

    fe validation

--- public/js/login.js
+++ public/js/login.js
@@ -3,14 +3,31 @@ const BASE_URL = 'http://localhost:3000';
 
 const formEl = document.getElementById('login');
 const errroEl = document.getElementById('err');
+const emailInputEl = formEl.elements.email;
+const passwordInputEl = formEl.elements.password;
 
 formEl.addEventListener('submit', async (event) => {
   event.preventDefault();
   console.log('js submit form');
+  clearErrors();
   const loginObj = {
     email: formEl.elements.email.value.trim(),
     password: formEl.elements.password.value.trim(),
   };
+
+  // TODO front end validation
+  if (checkInputObj(loginObj)) {
+    console.log('checkInputObj ===');
+    const errorsArr = [];
+    // paimti loginObj ir patikrinti
+    // 1. ar reiksmes yra (ar nelygu '')
+    // jei lygu tada formuojam klaida
+    // 2.paziureti ar nera ivesties laukas netrumpesins uz 4 simbolius
+    // jei taip tai {messge: 'too short', field:'password'}
+    // handleError(errorsArr);
+    return;
+  }
+
   console.log('loginObj ===', loginObj);
 
   const resp = await fetch(`${BASE_URL}/login`, {
@@ -34,7 +51,7 @@ formEl.addEventListener('submit', async (event) => {
     // nunaviguoja, padaro kad negaletume gryzti atgal psl su back
     window.location.replace('books.html');
   } else {
-    console.log('login fail');
+    console.log('login fail', dataInJs);
     handleError(dataInJs);
   }
 });
@@ -45,8 +62,35 @@ function handleError(msg) {
     errroEl.textContent = msg;
   }
   if (Array.isArray(msg)) {
+    // paprastas budas avaizduoti visas klaidas
+    // msg.forEach((eObj) => {
+    //   errroEl.innerHTML += `${eObj.message}<br>`;
+    // });
+    // atvaizduoti individualias klaidas
     msg.forEach((eObj) => {
-      errroEl.innerHTML += `${eObj.message}<br>`;
+      if (eObj.field === 'email') {
+        emailInputEl.style.border = '1px solid red ';
+        emailInputEl.nextElementSibling.textContent = eObj.message;
+      }
+      if (eObj.field === 'password') {
+        passwordInputEl.style.border = '1px solid red ';
+        passwordInputEl.nextElementSibling.textContent = eObj.message;
+      }
     });
+  }
+}
+
+function clearErrors() {
+  emailInputEl.style.border = 'none';
+  passwordInputEl.nextElementSibling.textContent = '';
+  passwordInputEl.style.border = 'none';
+  emailInputEl.nextElementSibling.textContent = '';
+}
+
+function checkInputObj(obj) {
+  for (key in obj) {
+    const value = obj[key];
+    console.log('value ===', value);
+    if (value === '') return true;
   }
 }
